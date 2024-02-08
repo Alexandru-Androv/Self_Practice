b9355e6554911332b1d1f8586eacbfe82ac18f68
alexa <alexandrovalexandru95@gmail.com>
2024-02-08 16:32:54 -0700

    day 13

---
+++ src/jd13_inheritance/_student/StudentClients.java
@@ -0,0 +1,26 @@
+package jd13_inheritance._student;
+
+public class StudentClients {
+    public static void main(String[] args) {
+        GraduateStudent student1 = new GraduateStudent("Alex",21, "male",
+                "221","programing", 'C', "Cydeo");
+        student1.study();
+        student1.grade();
+        System.out.println(student1);
+
+        System.out.println();
+
+        CydeoStudents student2 = new CydeoStudents("Jenn", 20, "female", "222",
+                "programing", 'A',"Cydeo", 33,
+                06, "Java" );
+        student2.study();
+        System.out.println(student2);
+    }
+
+}
+/*
+Create a class named "StudentClients":
+   - Create multiple objects representing different students.
+   - Test the methods and attributes of each object.
+
+ */
